da9a719ce0e59a5aa70bd3f6d91fa98061e6aa0a
BCBeno <circiubeni@gmail.com>
2025-12-04 16:19:25 +0200

    First Commit

---
+++ src/main/java/com/beno/summaryspherebackend/SummarySphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beno.summaryspherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarySphereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummarySphereBackendApplication.class, args);
+    }
+
+}
