596aaa6101e43b5761c10a74e7cd2b20badaceb2
BCBeno <circiubeni@gmail.com>
2026-09-01 19:50:40 +0300

    Tika file extraction limit

--- src/main/java/com/beno/summaryspherebackend/services/impl/FileExtractionServiceImpl.java
+++ src/main/java/com/beno/summaryspherebackend/services/impl/FileExtractionServiceImpl.java
@@ -8,10 +8,32 @@ import com.beno.summaryspherebackend.services.FileExtractionService;
 @Service
 public class FileExtractionServiceImpl implements FileExtractionService {
 
-    private final Tika tika = new Tika();
+    private static final int MAX_EXTRACTED_CHARACTERS = 1_000_000;
+
+    private final Tika tika;
+
+    public FileExtractionServiceImpl() {
+        this.tika = new Tika();
+        this.tika.setMaxStringLength(MAX_EXTRACTED_CHARACTERS + 1);
+    }
 
     @Override
     public String extractText(InputStream inputStream) throws Exception {
-        return tika.parseToString(inputStream);
+        String text = tika.parseToString(inputStream);
+
+        if (text.length() > MAX_EXTRACTED_CHARACTERS) {
+            throw new IllegalArgumentException(
+                    "Document contains too much text. Maximum: "
+                    + MAX_EXTRACTED_CHARACTERS + " characters"
+            );
+        }
+
+        if (text.isBlank()) {
+            throw new IllegalArgumentException(
+                    "No readable text could be extracted from the document"
+            );
+        }
+
+        return text;
     }
 }
